c755bbb973793005013c6ecfa93d85bf6801969c
drib861204 <56364972+drib861204@users.noreply.github.com>
2021-11-16 04:07:12 +0800

    1116

--- Pendulum.py
+++ Pendulum.py
@@ -13,26 +13,11 @@ Author: [openai](https://github.com/openai)
 Last modified: 2021/10/31
 """
 import pygame
-from math import pi, sin, cos, floor
+from math import pi, sin, cos
 import numpy as np
 
 
 class Pendulum:
-    #height = 600
-    #width = 800
-    #gravity = 0.01
-
-    #win = pygame.display.set_mode((width, height))
-    #pygame.display.set_caption("Pendulum Simulation")
-
-    #objects_group = []
-
-    #pygame.font.init()
-
-    #debug_font = pygame.font.SysFont('Bauhuas 93', 30)
-    #hint_font = pygame.font.SysFont('Bauhaus 93', 26)
-
-
     def __init__(self):
         self.theta_rod = 0
         self.theta_wheel = 0
@@ -59,19 +44,7 @@ class Pendulum:
         self.screen = pygame.display.set_mode((width, height))
         pygame.display.set_caption("Pendulum Simulation")
 
-
         '''
-        self.ball_x = 20
-        self.ball_y = 20
-        self.orn_x = width//2
-        self.orn_y = height//2
-        self.radius = 15
-        self.color = (224, 36, 1)
-        #self.origin = Origin(width // 2, self.y)
-        self.angle = pi / 4
-        self.angle_velocity = .1
-        self.angle_acceleration = 0
-        self.len = 200
         self.timer = 0
         '''
 
@@ -147,97 +120,3 @@ class Pendulum:
 def angle_normalize(th):
     return ((th+pi)%(2*pi))-pi
 
-
-def draw_text(text, font, text_col, x, y):
-    img = font.render(text, True, text_col)
-    win.blit(img, (x, y))
-
-
-def draw(self):
-    # global gravity
-    force = gravity * sin(self.angle)
-    self.angle_acceleration = (-1 * force) / self.len
-    self.angle_velocity += self.angle_acceleration
-    self.angle += self.angle_velocity
-
-    self.x = self.len * sin(self.angle) + self.origin.x
-    self.y = self.len * cos(self.angle) + self.origin.y
-
-    self.origin.draw()
-    pygame.draw.line(win, (216, 233, 168), (self.origin.x, self.origin.y), (self.x, self.y))
-    pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
-
-    self.timer += 1
-
-    draw_text(f"Current gravity: {gravity}", debug_font, (255, 255, 255), width // 2, 30)
-    draw_text(f"Current acceleration: {self.angle_acceleration}", debug_font, (255, 255, 255), width // 2, 60)
-    draw_text(f"Current momentum: {self.angle_velocity}", debug_font, (255, 255, 255), width // 2, 90)
-    draw_text(f"Press up or down key to change length! ", hint_font, (255, 255, 255), 10, 30)
-    draw_text(f"Current length: {self.len} ", hint_font, (255, 255, 255), 10, 60)
-
-    if self.timer > 40:
-        if gravity <= 160:
-            if gravity >= 10:
-                gravity += 2
-                gravity = floor(gravity)
-            else:
-                if gravity >= 1:
-                    gravity += 1
-                    gravity = floor(gravity)
-                else:
-                    gravity += .10
-        self.timer = 0
-
-
-def Origin(self, x, y, color=(216, 233, 168)):
-    self.x = x
-    self.y = y
-    self.radius = 10
-    self.color = color
-
-def draw_origin(self):
-    pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
-
-
-
-def update(window, obj_group):
-    win.fill((25, 26, 25))
-    for obj in obj_group:
-        obj.draw()
-    pygame.display.update()
-
-def main():
-    global gravity
-    run = True
-    clock = pygame.time.Clock()
-
-    # p_origin = Origin(width // 2, height // 2 - 200)
-    pendulum = Pendulum(20, height // 2 - 200, 450)
-    # objects_group.append(p_origin)
-    objects_group.append(pendulum)
-
-    while run:
-        clock.tick(60)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-                pygame.quit()
-            key = pygame.key.get_pressed()
-            if key[pygame.K_ESCAPE]:
-                run = False
-                pygame.quit()
-            if key[pygame.K_UP]:
-                pendulum.len -= 10
-            if key[pygame.K_DOWN]:
-                pendulum.len += 10
-            if key[pygame.K_r]:
-                # reset pendulum position, physics
-                pendulum.angle_velocity = .1
-                pendulum.angle_acceleration = 0
-                pendulum.angle = pi / 4
-                pendulum.timer = 0
-                gravity = .01
-
-        update(win, objects_group)
-
-# main()
